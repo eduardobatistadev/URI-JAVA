a03f1c530e32c99f157e01bd1055290bd7ba08c5
dueduardoful <dueduardoful@gmail.com>
2019-12-25 17:00:40 -0300

    alterações basicas feitas

--- src/application/Uri_1002.java
+++ src/application/Uri_1002.java
@@ -5,20 +5,20 @@ import java.util.Scanner;
 
 public class Uri_1002 {
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double n = 3.14159;
+		double raio;
+		
+		System.out.println();
+		raio = sc.nextDouble();
+		double area = n*raio*raio;
+		
+		System.out.printf("A=%.4f", area);
+		System.out.println();
 	
-	Locale.setDefault(Locale.US);
-	Scanner sc = new Scanner(System.in);
-	
-	double n = 3.14159;
-	double raio;
-	
-	System.out.println();
-	raio = sc.nextDouble();
-	double area = n*raio*raio;
-	
-	System.out.printf("A=%.4f", area);
-	System.out.println();
-	
-	sc.close();
-}
+		sc.close();
+
+	}
 }
--- src/application/Uri_1003.java
+++ src/application/Uri_1003.java
@@ -5,18 +5,18 @@ import java.util.Scanner;
 public class Uri_1003 {
 
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		
 		int A = sc.nextInt();
 		int B = sc.nextInt();
 		int SOMA = A+B;
 		
-		System.out.println("SOMA = " + SOMA);
-		
+		System.out.printf("SOMA = " + SOMA);
+		System.out.println();
 		
 		
 		sc.close();
-		
 
 	}
 
