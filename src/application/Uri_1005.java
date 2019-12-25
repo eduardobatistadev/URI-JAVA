22706ff1775425805e5c82c79e196cdb482a444b
dueduardoful <dueduardoful@gmail.com>
2019-12-25 17:39:43 -0300

    alteracao

--- src/application/Uri_1005.java
+++ src/application/Uri_1005.java
@@ -1,24 +1,24 @@
 package application;
+
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Uri_1005 {
 
 	public static void main(String[] args) {
-		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
 		double A = sc.nextDouble();
 		double B = sc.nextDouble();
-
-		double MEDIA = (A*3.5+B*7.5)/11;
-
+		
+		double MEDIA = (A*3.5 + B*7.5)/11;
+		
 		System.out.printf("MEDIA = %.5f", MEDIA);
 		System.out.println();
 		
-		
 		sc.close();
+		
 
 	}
 
