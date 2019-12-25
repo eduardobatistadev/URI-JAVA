fb5b75da6938218aab57c8d6ec760d3869e40fac
dueduardoful <dueduardoful@gmail.com>
2019-12-25 15:32:59 -0300

    Projeto criado e codificado

---
+++ src/application/Uri_1004.java
@@ -0,0 +1,22 @@
+package application;
+
+import java.util.Scanner;
+
+public class Uri_1004 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int PROD = A*B;
+		
+		System.out.println("PROD = " + PROD);
+		
+		
+		sc.close();
+
+	}
+
+}
