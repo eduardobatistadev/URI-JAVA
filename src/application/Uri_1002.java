ea1e36ebe2365b5e0430138704f354ead638e274
dueduardoful <dueduardoful@gmail.com>
2019-12-25 14:08:25 -0300

    projeto terminado

--- src/application/Uri_1002.java
+++ src/application/Uri_1002.java
@@ -1,5 +1,24 @@
 package application;
 
-public class Uri_1002 {
+import java.util.Locale;
+import java.util.Scanner;
 
+public class Uri_1002 {
+	public static void main(String[] args) {
+	
+	Locale.setDefault(Locale.US);
+	Scanner sc = new Scanner(System.in);
+	
+	double n = 3.14159;
+	double raio;
+	
+	System.out.println();
+	raio = sc.nextDouble();
+	double area = n*raio*raio;
+	
+	System.out.printf("A=%.4f", area);
+	System.out.println();
+	
+	sc.close();
+}
 }
