2cb4eab529e68e5d30fb2e2a971c0109c23688ab
dueduardoful <dueduardoful@gmail.com>
2019-12-25 14:01:44 -0300

    programa terminado

--- src/application/Uri_1001.java
+++ src/application/Uri_1001.java
@@ -1,9 +1,22 @@
 package application;
 
+import java.util.Scanner;
+
 public class Uri_1001 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int X = A + B;
+		
+		System.out.println("X = " + X);
+		
+		
+		
+		sc.close();
 
 	}
 
