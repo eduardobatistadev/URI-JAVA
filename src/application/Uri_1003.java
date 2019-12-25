0c5d9c03d8f3e972dd0ababd7dad07c13bba38a2
dueduardoful <dueduardoful@gmail.com>
2019-12-25 15:14:36 -0300

    projeto criado e codificado

---
+++ src/application/Uri_1003.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.util.Scanner;
+
+public class Uri_1003 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int SOMA = A+B;
+		
+		System.out.println("SOMA = " + SOMA);
+		
+		
+		
+		sc.close();
+		
+
+	}
+
+}
